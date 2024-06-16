a84208d3c3708c88db5635de70b123219dc83e18
EloSatis <JustANormalMonkeVR@gmail.com>
2024-06-16 16:35:57 -0500

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,59 @@
+﻿using BepInEx;
+
+
+using HarmonyLib;
+using Loading;
+using System;
+using System.Diagnostics;
+using UnityEngine;
+using static stopWatch.Plugin;
+using Object = UnityEngine.Object;
+
+namespace stopWatch
+{
+    [BepInPlugin(Name, GUID, Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string Name = "Satis Stop watch";
+        public const string GUID = "org.Satis.Stop.Watch";
+        public const string Version = "1.0";
+
+        private bool patchedHarmony = false;
+        private static GameObject Gameobject;
+        [System.Serializable]
+        public class LoginData
+        {
+            public string license;
+
+        }
+        void Awake()
+        {
+            if (!patchedHarmony && Loader.loaded == false)
+            {
+                Harmony harmony = new Harmony(GUID);
+                harmony.PatchAll();
+                patchedHarmony = true;
+                Loader.loaded = true;
+
+            }
+        }
+    }
+    [HarmonyPatch(typeof(GorillaLocomotion.Player), "FixedUpdate")]
+    internal class UpdatePatch
+    {
+        private static bool alreadyInit;
+        public static GameObject Gameobject;
+
+        static void Postfix()
+        {
+           
+            if (!alreadyInit)
+            {
+                alreadyInit = true;
+                Gameobject = new GameObject();
+                
+                Object.DontDestroyOnLoad(Gameobject);
+            }
+        }
+    }
+}
